6f38b8a31925da4b35f5c7c2e43e74364ea0ee29
Marc Blais <marcantblais@gmail.com>
2023-08-31 18:49:45 -0400

    fix for last push

--- app/components/RecordingList.tsx
+++ app/components/RecordingList.tsx
@@ -72,6 +72,7 @@ const RecordingList = (
     
     const removeScroll = (e: React.TouchEvent) => {
         const recordingsList = recordingsListRef.current
+        const body = document.body
 
         if (!recordingsList)
             return
@@ -84,9 +85,12 @@ const RecordingList = (
             const position = e.touches[0].clientY
             const recordingsListScroll = recordingsList.scrollTop
 
-            if (recordingsListScroll <= 0 && start - position <= 0) {
+            if (position - start >= body.clientHeight / 10 && recordingsListScroll <= 0) {
+                e.stopPropagation
                 recordingsList.classList.add("overflow-hidden")
+            } else if (recordingsListScroll <= 0 && start - position <= 0) {
                 foldRecordingsList()
+                recordingsList.classList.add("overflow-hidden")
             } else if (scrollHeight - recordingsListScroll <= height && start - position >= 0)
                 recordingsList.classList.add("overflow-hidden")
             else if (recordingsListScroll <= 0 && start - position > 0) {
