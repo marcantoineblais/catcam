f3ca8fbd292364e2953181326bf93481eca68c32
Marc Blais <marcantblais@gmail.com>
2023-09-04 08:04:02 -0400

    changed time for auto dark mode

--- app/components/SettingsManager.tsx
+++ app/components/SettingsManager.tsx
@@ -17,7 +17,7 @@ export default function SettingsManager({ session, setPageSize }: { session: any
         else if (appearance === "auto") {
             const time = new Date(Date.now()).getHours()
             
-            if (time >= 20 || time <= 8)
+            if (time >= 20 || time < 8)
                 document.body.classList.add("dark")
             else
                 document.body.classList.remove("dark")
