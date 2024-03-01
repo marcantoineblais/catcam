3f72b185660eafd447922caeb0c65d9d9e312039
Marc Blais <marcantblais@gmail.com>
2024-03-01 01:05:52 -0500

    removed some tracked files

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  env: {
-      geolocationAPIKey: "30358e5d04ce4b68981bf475c322a667",
-      serverUrl: "https://catcam.source.marchome.xyz" 
-  },
-}
-
-module.exports = nextConfig
