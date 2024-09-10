1bd0f204cd88a903b74fbbf562bd0e6f160ee1f8
Marc Blais <marcantblais@gmail.com>
2024-09-10 17:53:45 -0400

    updated cookie storage

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -65,31 +65,33 @@ export default function Login() {
     }
 
     return (
-        <main className="h-full px-1 pt-5 max-w-screen-md container mx-auto flex flex-col justify-between items-center">
-            <form onSubmit={(e) => submitForm(e)} className="w-full px-3 py-6 shadow bg-gray-50 rounded dark:bg-zinc-700 dark:shadow-zinc-50/10" ref={formRef} autoComplete="on">
-                <h1 className="w-full pb-10 text-center text-3xl paysage-hidden">Login</h1>
+        <div className="h-full flex flex-col overflow-hidden">
+            <main className="h-full px-1 pt-5 max-w-screen-md container mx-auto">
+                <form onSubmit={(e) => submitForm(e)} className="w-full px-3 py-6 shadow bg-gray-50 rounded dark:bg-zinc-700 dark:shadow-zinc-50/10" ref={formRef} autoComplete="on">
+                    <h1 className="w-full pb-10 text-center text-3xl paysage-hidden">Login</h1>
 
-                <label className="flex pt-3">
-                    <p className="basis-32 text-sm">Email</p>
-                    <input className="px-1.5 grow bg-gray-100 rounded text-sm dark:text-zinc-950" name="email"></input>
-                </label>
+                    <label className="flex pt-3">
+                        <p className="basis-32 text-sm">Email</p>
+                        <input className="px-1.5 grow bg-gray-100 rounded text-sm dark:text-zinc-950" name="email"></input>
+                    </label>
 
-                <label className="flex pt-3">
-                    <p className="basis-32 text-sm">Password</p>
-                    <input className="px-1.5 grow bg-gray-100 rounded text-sm dark:text-zinc-950" name="password" type="password"></input>
-                </label>
+                    <label className="flex pt-3">
+                        <p className="basis-32 text-sm">Password</p>
+                        <input className="px-1.5 grow bg-gray-100 rounded text-sm dark:text-zinc-950" name="password" type="password"></input>
+                    </label>
 
-                <label className="flex pt-3">
-                    <p className="basis-32 text-sm">Remember me</p>
-                    <input className="bg-gray-100 rounded" id="remember-me dark:text-zinc-950" name="rememberMe" type="checkbox"></input>
-                </label>
+                    <label className="flex pt-3">
+                        <p className="basis-32 text-sm">Remember me</p>
+                        <input className="bg-gray-100 rounded" id="remember-me dark:text-zinc-950" name="rememberMe" type="checkbox"></input>
+                    </label>
 
-                <div className="pt-5 flex justify-center">
-                    <button type="submit" className="py-2 w-32 bg-sky-800 text-gray-50 rounded duration-200 hover:bg-sky-700">Submit</button>
-                </div>
-            </form>
+                    <div className="pt-5 flex justify-center">
+                        <button type="submit" className="py-2 w-32 bg-sky-800 text-gray-50 rounded duration-200 hover:bg-sky-700">Submit</button>
+                    </div>
+                </form>
+            </main>
 
             <Logo className="text-gray-950 dark:text-zinc-200 translate-y-1/2 scale-150" />
-        </main>
+        </div>
     );
 }
