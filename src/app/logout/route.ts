171c748d7f2858f15b6cec747f2f04986ac6f98b
Marc Blais <marcantblais@gmail.com>
2024-09-12 19:23:38 -0400

    added service worker

--- src/app/logout/route.ts
+++ src/app/logout/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 export function GET(request: NextRequest) {
-    const response = NextResponse.redirect(new URL("/login", request.url));
+    const response = NextResponse.redirect(new URL("/login", "https://catcam.marchome.xyz"));
     
     response.cookies.set({
         name: "session",
