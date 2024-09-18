ee5ea9c52d2a6fc4b092525819c28a3586d9ed41
Marc Blais <marcantblais@gmail.com>
2024-09-18 01:16:13 -0400

    added basic transport

--- src/components/video/VideoPlayerOverlay.tsx
+++ src/components/video/VideoPlayerOverlay.tsx
@@ -23,6 +23,18 @@ export default function VideoPlayerOverlay(
         videoRef: React.RefObject<HTMLVideoElement>;
     }) {
 
+    const [updatedTime, setUpdatedTime] = React.useState<number | null>(null);
+    const [isCurrentlyPlaying, setIsCurrentlyPlaying] = React.useState<boolean>(false);
+    const [displayedTime, setDisplayedTime] = React.useState<number | null>(null);
+
+    React.useEffect(() => {
+        if (updatedTime !== null) {
+            setDisplayedTime(updatedTime);
+        } else {
+            setDisplayedTime(currentTime || null);
+        }
+    }, [currentTime, updatedTime]);
+
     function pause() {
         const video = videoRef.current;
 
@@ -45,6 +57,77 @@ export default function VideoPlayerOverlay(
         } catch (_) { }
     }
 
+    function updateCurrentTime(target: HTMLDivElement, clientX: number) {
+        if (!duration)
+            return 0;
+
+        const width = target.clientWidth;
+        const positionOffset = target.getBoundingClientRect().left;
+        let updatedTime = (clientX - positionOffset) / width * duration;
+
+        if (updatedTime < 0)
+            updatedTime = 0;
+
+        if (updatedTime > duration)
+            updatedTime = duration;
+
+        return updatedTime;
+    }
+
+    function onMouseDown(e: React.MouseEvent<HTMLDivElement>) {
+        if (isPlaying) {
+            pause();
+        }
+
+        const updatedTime = updateCurrentTime(e.currentTarget, e.clientX);
+        setIsCurrentlyPlaying(isPlaying || false);
+        setUpdatedTime(updatedTime);
+    }
+
+    function onTouchStart(e: React.TouchEvent<HTMLDivElement>) {
+        if (e.touches.length > 1)
+            return;
+        
+        if (isPlaying) {
+            pause();
+        }
+
+        const updatedTime = updateCurrentTime(e.currentTarget, e.touches[0].clientX);
+        setIsCurrentlyPlaying(isPlaying || false);
+        setUpdatedTime(updatedTime);
+    }
+
+    function onMouseMove(e: React.MouseEvent<HTMLDivElement>) {
+        if (updatedTime === null)
+            return;
+
+        const time = updateCurrentTime(e.currentTarget, e.clientX);
+        setUpdatedTime(time);
+    }
+
+    function onTouchMove(e: React.TouchEvent<HTMLDivElement>) {
+        if (e.touches.length > 1)
+            return;
+
+        if (updatedTime === null)
+            return;
+
+        const time = updateCurrentTime(e.currentTarget, e.touches[0].clientX);
+        setUpdatedTime(time);
+    }
+
+    function onMouseUp() {
+        const video = videoRef.current;
+
+        if (!video || updatedTime === null)
+            return;
+
+        setUpdatedTime(null);
+        video.currentTime = updatedTime;
+        if (isCurrentlyPlaying)
+            play();
+    }
+
     function normaliseTime(value: number) {
         const valueStr = value.toFixed();
         return valueStr.length < 2 ? "0" + valueStr : valueStr;
@@ -58,20 +141,20 @@ export default function VideoPlayerOverlay(
     }
 
     function renderTime() {
-        if (currentTime === undefined || duration === undefined)
+        if (displayedTime === null || duration === undefined)
             return "";
 
-        return `${displayTime(currentTime)} / ${displayTime(duration)}`;
+        return `${displayTime(displayedTime)} / ${displayTime(duration)}`;
     }
 
     function renderDurationBar() {
-        if (currentTime === undefined || duration === undefined)
+        if (displayedTime === null || duration === undefined)
             return null;
 
         return (
             <div
                 className="absolute top-0 bottom-0 left-0 bg-sky-700 rounded"
-                style={{ width: `${currentTime / duration * 100}%` }}
+                style={{ width: `${displayedTime / duration * 100}%` }}
             ></div>
         );
     }
@@ -89,13 +172,13 @@ export default function VideoPlayerOverlay(
     }
 
     function renderTrackingHead() {
-        if (currentTime === undefined || duration === undefined)
+        if (displayedTime === null || duration === undefined)
             return null;
 
         return (
             <div
                 className="absolute h-5 w-5 -top-2 -translate-x-2 bg-gray-100 rounded-full cursor-pointer dark:bg-zinc-200"
-                style={{ left: `${currentTime / duration * 100}%` }}
+                style={{ left: `${displayedTime / duration * 100}%` }}
             ></div>
         );
     }
@@ -104,12 +187,21 @@ export default function VideoPlayerOverlay(
         <div
             className="absolute invisible opacity-0 inset-0 duration-500 text-gray-50 dark:text-zinc-200 data-[loaded]:visible data-[loaded]:opacity-100"
             data-loaded={isLoaded ? true : undefined}
+            onMouseUp={onMouseUp}
+            onMouseLeave={onMouseUp}
         >
             <div onClick={(e) => e.stopPropagation()} className="max-w-full px-5 py-1.5 absolute bottom-0 left-0 right-0 flex flex-col justify-between items-center bg-gray-950/75">
                 {
                     !isLive && (
                         <div className="py-3 w-full flex justify-center">
-                            <div className="h-1 w-full relative bg-gray-800 rounded cursor-pointer dark:bg-zinc-800">
+                            <div
+                                className="h-1 w-full relative bg-gray-800 rounded cursor-pointer dark:bg-zinc-800"
+                                onMouseDown={onMouseDown}
+                                onMouseMove={onMouseMove}
+                                onTouchStart={onTouchStart}
+                                onTouchMove={onTouchMove}
+                                onTouchEnd={onMouseUp}
+                            >
                                 {renderBufferBar()}
                                 {renderDurationBar()}
                                 {renderTrackingHead()}
