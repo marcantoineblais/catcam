5acdf205456c47a76c7b70f751e0c7d5a95efcd5
Marc Blais <blaisma@live.fr>
2025-05-16 21:01:46 -0400

    updated dark theme btn color

--- src/components/QualityButton.tsx
+++ src/components/QualityButton.tsx
@@ -19,7 +19,7 @@ export default function QualityButton({
     >
       <div className="w-full h-full flex gap-1">
         <div
-          className="w-full h-full flex text-sm font-bold bg-gray-700 text-gray-50 dark:bg-zinc-300 dark:text-zinc-900 duration-500 delay-500 ease-in-out data-hq:bg-sky-700 data-hq:dark:bg-sky-700 data-hq:text-white"
+          className="w-full h-full flex text-sm font-bold bg-gray-700 dark:bg-zinc-700 duration-500 delay-500 ease-in-out data-hq:bg-sky-700 data-hq:dark:bg-sky-700 data-hq:text-white"
           data-hq={isHQ ? true : undefined}
         >
           <div
@@ -29,7 +29,7 @@ export default function QualityButton({
             HQ
           </div>
 
-          <div className="z-10 w-9 h-9 rounded-full bg-white inset-ring-2 inset-ring-black/10 dark:bg-zinc-700 dark:inset-ring-white/10 origin-center scale-105"></div>
+          <div className="z-10 w-9 h-9 rounded-full bg-white inset-ring-2 inset-ring-black/10 dark:bg-zinc-900 dark:inset-ring-white/10 origin-center scale-105"></div>
 
           <div
             className="px-1.5 w-10 max-w-[2.5rem] h-full flex justify-end items-center rounded-r-full overflow-hidden duration-1000 ease-in-out data-hq:w-0 data-hq:max-w-0 data-hq:px-0"
