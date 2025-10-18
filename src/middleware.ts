4d0a202aa6f96c3a30bd4b6dea4db4e57e6f8268
Marc Blais <blaisma@live.fr>
2025-10-17 22:38:22 -0400

    added api auth logout to public route

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { getToken } from "./libs/jwt";
 
-const publicRoutes = ["/login", "/logout", "/api/auth/login", "/api/auth/session"];
+const publicRoutes = ["/login", "/logout", "/api/auth/login", "/api/auth/logout", "/api/auth/session"];
 
 export async function middleware(request: NextRequest) {
   const requestedPath = request.nextUrl.pathname;
