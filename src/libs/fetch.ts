33c739d63afdc89805dcc4d6ecd910c7965abdc0
Marc Blais <blaisma@live.fr>
2025-10-19 01:33:13 -0400

    handle redirection properly on token revocation

--- src/libs/fetch.ts
+++ src/libs/fetch.ts
@@ -44,7 +44,7 @@ export async function fetchMonitors({
 
     if (response.ok) {
       const data = await response.json();
-      if (!data.ok) {
+      if (data.ok === false) {
         console.error("[FetchMonitors] Token was revoked by server. User needs to log in again.");
         throw new Error("Invalid monitors data");
       }
@@ -91,7 +91,7 @@ export async function fetchVideos(
 
     if (response.ok) {
       const data = await response.json();
-      if (!data.ok) {
+      if (data.ok === false) {
         console.error("[FetchVideos] Token was revoked by server. User needs to log in again.");
         throw new Error("Invalid Videos data");
       }
