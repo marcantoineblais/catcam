34670f97d193bb2b2aa94ef1aefcaa07db284cd2
Marc Blais <blaisma@live.fr>
2025-12-09 10:51:13 -0500

    updated logout method

--- src/app/api/auth/logout/route.ts
+++ src/app/api/auth/logout/route.ts
@@ -1,8 +1,9 @@
+import { SESSION_COOKIE_NAME } from "@/src/config";
 import { NextResponse } from "next/server";
 
 export function GET() {
   const response = NextResponse.json({ ok: true });
-  response.cookies.set("session", "", {
+  response.cookies.set(SESSION_COOKIE_NAME, "", {
     maxAge: 0,
     path: "/",
   });
