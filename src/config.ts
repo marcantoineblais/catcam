863ec748fa50d61e08192db888fce7dc9243c676
Marc Blais <blaisma@live.fr>
2026-02-09 18:11:07 -0500

    added reference to server timezone

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const isProd = process.env.NODE_ENV === "production";
-export const DOMAIN_NAME = process.env.NEXT_PUBLIC_DOMAIN_NAME!;
+export const DOMAIN_NAME = process.env.DOMAIN_NAME!;
 export const SERVER_URL = process.env.SERVER_URL!;
 export const AUTO_DARK_MODE_TIME = { start: 19, end: 7 };
 export const DEFAULT_SETTINGS = {
